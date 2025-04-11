3a9fd4b5d15294399befe8fcac47701f90603447
Nashpae <nashipaelenkupae@gmail.com>
2025-04-11 21:05:04 +0300

    Add files via upload

---
+++ user_list.php
@@ -0,0 +1,14 @@
+<?php
+include 'db_connect.php';
+
+$result = $conn->query("SELECT name, email FROM users");
+if ($result->num_rows > 0) {
+    echo "<ul>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<li>" . htmlspecialchars($row['name']) . " (" . htmlspecialchars($row['email']) . ")</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "No users found.";
+}
+?>
